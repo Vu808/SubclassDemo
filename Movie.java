7b742c48d1500577e436e663710aef18890b43fb
Vu808 <31222309+Vu808@users.noreply.github.com>
2017-08-23 00:21:48 -1000

    Add files via upload

--- Movie.java
+++ Movie.java
@@ -1,4 +1,32 @@
 
-public class Movie {
+public class Movie extends Entertainment {
 
+	
+	private String genre;
+	private String movieRating;
+	
+	
+	public Movie(String name, String whatKind, String maturity, String theme) {
+		
+		super(name, whatKind);
+		
+		movieRating = maturity;
+		genre = theme;
+	}
+	
+	public String getMovieRating() {
+		return movieRating;
+	}
+	
+	public int getGenre() {
+		return genre;
+	}
+	
+	public void setMovieRating(string maturity) {
+	movieRating = maturity;
+	}
+	
+	public void setGenre(String theme) {
+		genre = theme;
+	}
 }
--- VideoGame.java
+++ VideoGame.java
@@ -1,4 +1,28 @@
 
-public class VideoGame {
+public class VideoGame extends Entertainment {
+	
+	private String gameRating;
+	private String genre;
+	
+	public VideoGame(String name, String whatKind, String maturity, String theme) {
+		super(name, whatKind);
+		gameRating = maturity;
+		genre = theme;
+	}
 
+	public String getGameRating() {
+		return gameRating;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	public void setGameRating(String maturity) {
+		gameRating = maturity;
+	}
+	
+	public void setGenre(String theme) {
+		genre = theme;
+	}
 }
